bdcc29764105c2788ea5ac76923f4fde627b8654
alek vt <alek.vtichelen@gmail.com>
2016-10-05 17:41:59 +0200

    functional 'show all locations' function

--- app/_attachments/js/locationdrivel.js
+++ app/_attachments/js/locationdrivel.js
@@ -28,7 +28,7 @@ function logLocation(aLocation){
 		url: 'http://127.0.0.1:5984/sharealocation',
 		data: JSON.stringify(oData),
 		error: function(){ alert('things went wrong')},
-		succes: function(){ alert('things apparently went right')},
+		success: function(){ console.log('things apparently went right')},
 		contentType: 'application/json'
 	});
 	
@@ -36,5 +36,29 @@ function logLocation(aLocation){
 }
 
 function getAllLocations(){
-	alert('show all locations.');
+	
+	$.getJSON(
+		'http://127.0.0.1:5984/sharealocation/_design/all/_view/view_all',
+		function (data) {
+			var aData = data['rows'];
+			var length = aData.length;
+			for (var i = 0; i < length; i++) {
+				setPosition(aData[i].value);
+			}
+		}
+	);
+	
+	console.log('show all locations.');
+}
+
+function setPosition(aData){
+	var name = aData['person'];
+	var location = aData['location'];
+	if (typeof(location[0]) !== 'undefined' && location) {
+		addMarker([location[0], location[1]]);
+	}
+}
+
+function addMarker(position){
+	var marker = L.marker(position).addTo(map);
 }
